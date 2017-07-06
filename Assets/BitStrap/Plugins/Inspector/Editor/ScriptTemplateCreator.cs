e9285ae069ffff33712b110f673c2edea9401853
Philippos Slicher <philipposslicher@gmail.com>
2017-07-06 21:45:54 +0300

    Bug Fixes, new menu music

---
+++ Assets/BitStrap/Plugins/Inspector/Editor/ScriptTemplateCreator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.ProjectWindowCallback;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace UnityEditor.ProjectWindowCallback
+{
+	internal sealed class CreateScriptAssetAction : EndNameEditAction
+	{
+		public override void Action( int instanceId, string path, string source )
+		{
+			BitStrap.ScriptTemplateCreator.WriteScript( path, source, true );
+		}
+	}
+}
+
+namespace BitStrap
+{
+	public sealed class ScriptModificationProcessor : UnityEditor.AssetModificationProcessor
+	{
+		public static void OnWillCreateAsset( string path )
+		{
+			path = path.Replace( ".meta", "" );
+			if( !path.EndsWith( ".cs" ) || !File.Exists( path ) )
+				return;
+			
+			ScriptTemplateCreator.WriteScript( path, ScriptTemplatePreferences.CSharpScriptDefaultCode, false );
+		}
+	}
+
+	public static class ScriptTemplateCreator
+	{
+		private static bool justCreatedCustomScript = false;
+
+		private static Texture2D ScriptIcon
+		{
+			get { return EditorGUIUtility.IconContent( "cs Script Icon" ).image as Texture2D; }
+		}
+
+		public static void WriteScript( string path, string source, bool isCustomScript )
+		{
+			if( !isCustomScript && justCreatedCustomScript )
+				return;
+
+			justCreatedCustomScript = isCustomScript;
+
+			string className = Path.GetFileNameWithoutExtension( path );
+			source = source.Replace( "#CLASSNAME#", className );
+			source = source.Replace( "#SCRIPTNAME#", className );
+
+			int currentYear = DateTime.Today.Year;
+			source = source.Replace( "#YEAR#", currentYear.ToString() );
+
+			File.WriteAllText( path, source );
+
+			AssetDatabase.ImportAsset( path );
+			Object o = AssetDatabase.LoadAssetAtPath( path, typeof( Object ) );
+			ProjectWindowUtil.ShowCreatedAsset( o );
+		}
+
+		[MenuItem( "Assets/Create/C# Editor Script", false, 81 )]
+		public static void CreateCSharpEditorScript()
+		{
+			string path = GetNewScriptPath( "NewEditor" );
+			CreateScript( path, ScriptTemplatePreferences.CSharpEditorScriptDefaultCode );
+		}
+
+		private static string GetNewScriptPath( string scriptName )
+		{
+			string path = "Assets/";
+			if( Selection.activeObject != null )
+			{
+				path = AssetDatabase.GetAssetPath( Selection.activeObject );
+				if( !AssetDatabase.IsValidFolder( path ) )
+					path = Path.GetDirectoryName( path );
+			}
+
+			return Path.Combine( path, string.Concat( scriptName, ".cs" ) );
+		}
+
+		private static void CreateScript( string path, string source )
+		{
+			var createScriptAssetAction = ScriptableObject.CreateInstance<CreateScriptAssetAction>();
+			ProjectWindowUtil.StartNameEditingIfProjectWindowExists( 0, createScriptAssetAction, path, ScriptIcon, source );
+		}
+	}
+}
---
+++ Assets/BitStrap/Plugins/Monads/Option.cs
@@ -0,0 +1,99 @@
+﻿namespace BitStrap
+{
+	public static class Option
+	{
+		public static Option<T> Some<T>( T value )
+		{
+			return new Option<T>( value );
+		}
+	}
+
+	public struct Option<A>
+	{
+		public static readonly bool IsValueType;
+		public static readonly Option<A> None = new Option<A>();
+
+		static Option()
+		{
+			IsValueType = typeof( A ).IsValueType;
+		}
+
+		private readonly A value;
+
+		public readonly bool hasValue;
+
+		public Option( A value )
+		{
+			this.value = value;
+			hasValue = IsValueType || value != null;
+		}
+
+		public static implicit operator Option<A>( A value )
+		{
+			return new Option<A>( value );
+		}
+
+		public bool TryGet( out A retValue )
+		{
+			if( hasValue )
+			{
+				retValue = value;
+				return true;
+			}
+
+			retValue = default( A );
+			return false;
+		}
+
+		public B Match<B>( System.Func<A, B> onSome, System.Func<B> onNone )
+		{
+			if( hasValue )
+				return onSome( value );
+			return onNone();
+		}
+
+		public A Or( A orValue )
+		{
+			if( hasValue )
+				return value;
+
+			return orValue;
+		}
+
+		public A Or( System.Func<A> onNone )
+		{
+			if( hasValue )
+				return value;
+
+			return onNone();
+		}
+
+		public Option<B> Select<B>( System.Func<A, B> select )
+		{
+			if( !hasValue )
+				return Option<B>.None;
+
+			return select( value );
+		}
+
+		public Option<A> Where( System.Predicate<A> predicate )
+		{
+			if( !hasValue || !predicate( value ) )
+				return None;
+
+			return this;
+		}
+
+		public Option<C> SelectMany<B, C>( System.Func<A, Option<B>> func, System.Func<A, B, C> select )
+		{
+			if( !hasValue )
+				return Option<C>.None;
+
+			var b = func( value );
+			if( !b.hasValue )
+				return Option<C>.None;
+
+			return select( value, b.value );
+		}
+	}
+}
